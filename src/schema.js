f01fc15f7ff2e6a9280996958833f05e266e6428
tara z. manicsic <tzmanics@gmail.com>
2018-01-29 22:12:59 -0500

    add events & update schema

--- src/schema.js
+++ src/schema.js
@@ -18,3 +18,18 @@ const GDEType = new GraphQLObjectType({
     twitter: { type: new GraphQLNonNull(GraphQLString) }
   })
 });
+
+const Events = new GraphQLObjectType({
+  name: 'Events',
+  description: 'This respresents events GDEs are attending',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    date: { type: new GraphQLNonNull(GraphQLString) },
+    location: { type: new GraphQLNonNull(GraphQLString) },
+    gdes: {
+      type: GDEType,
+      resolve: (event) => _.flatMap(GDEs, gde => gde.id == event.gde_id);
+    }
+  })
+});
