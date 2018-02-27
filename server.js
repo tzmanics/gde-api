bf71b75c16838d46f3d591f9b57c27e9a243fc80
tzmanics <tzmanics@gmail.com>
2018-02-26 22:59:10 -0500

    change dirs & add mongoose

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const graphqlHTTP = require('express-graphql')
-const schema = require('./src/schema.js')
+const cors = require('cors')
+const schema = require('./graphql/schema.js')
+const mongoose = require('./config/mongoose')
+
 
 const PORT = process.env.PORT || 3000
 const app = express()
-app.use('/', graphqlHTTP({
+const DB = mongoose()
+
+app.use('*', cors())
+
+app.use('/graphql', cors(), graphqlHTTP({
   schema: schema,
   graphiql: true
 }))
