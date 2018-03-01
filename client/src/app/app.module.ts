ca93abc2fac956dd107bc089d07014971e7c05ae
tzmanics <tzmanics@gmail.com>
2018-03-01 10:53:07 +0200

    add graphql links & info to module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
+import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -13,9 +15,22 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    apollo.create({
+      link: httpLink.create({
+        uri: 'https://gde-api-jyxcnvxuuv.now.sh/graphql'
+      }),
+      cache: new InMemoryCache()
+    });
+  }
+}
